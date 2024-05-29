da6c60fde3055dd820c62072352712ccf12e9c3b
dev-mr <m.rizwan@crystal-lite.net>
2024-05-30 03:52:16 +0500

    Complete UK Page

--- lp/best-seo-agency/public/js/secondary.js
+++
@@ -1,4 +0,0 @@
-const emblaNodeForIndustries = document.querySelector('.__industries .embla')
-const optionsForIndustries = { loop: true, }
-const pluginsForIndustries = [EmblaCarouselClassNames()]
-const emblaApiForIndustries = EmblaCarousel(emblaNodeForIndustries, optionsForIndustries, pluginsForIndustries)
